2f548ede5d2307b9cf151395448a79dc18db4ac3
Suppakorn Chalermsripinyorat <gmgetter@gmail.com>
2026-06-16 14:33:31 +0700

    16/6 SECRET KEY

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET-KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
